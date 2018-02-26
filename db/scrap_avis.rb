3f104590ad018f8ab5dd0675bd77d8b89f1f63b5
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-26 17:48:12 +0100

    scrap car's names and descriptions

---
+++ db/scrap_avis.rb
@@ -0,0 +1,13 @@
+require 'open-uri'
+require 'nokogiri'
+
+page_avis_prestige = open('https://www.avis.fr/services-avis/v%C3%A9hicules-de-location/prestige/france').read
+# page_avis_tourism  = open('https://www.avis.fr/services-avis/v%C3%A9hicules-de-location/vehicules-de-tourisme').read
+
+html_prestige = Nokogiri::HTML(page_avis_prestige)
+# html_tourism  = Nokogiri::HTML(page_avis_tourism)
+
+html_prestige.search('.content-51b').each do |element|
+  element.search('h2').each { |e| p e.text.split(':').last.strip }                  # nom de la voiture
+  element.search('.changeFontSize').each { |e| p e.text.split("\s\s").first.strip } # description
+end
