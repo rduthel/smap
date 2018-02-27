89646ae4de3f21340e0c229e9a6ca7d394ecb1bb
thomas jouvel <thomas.jouvel@gmail.com>
2018-02-27 13:55:14 +0100

    automatic creation of DriverProfile when user Sign Up

--- app/models/driver_profile.rb
+++ app/models/driver_profile.rb
@@ -1,5 +1,5 @@
 class DriverProfile < ApplicationRecord
-  belongs_to :car
+  belongs_to :car, optional: true
   belongs_to :user
   has_many :additional_drivers
   has_many :addresses
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
   resources :cars, only: [:index, :show, :estimation, :subscription]
 
-  get "/profil", to: "driver_profile#profil"
+
+  get "/profil", to: "driver_profiles#profile"
+  get "/profil/edit", to: "driver_profiles#edit"
+  patch "/profil", to: "driver_profiles#update"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
