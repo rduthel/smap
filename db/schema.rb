c53d775b0ab47164e79c66b8580aaae8fb88dee8
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-27 17:42:42 +0100

    added a photo column in cars table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180227140322) do
+ActiveRecord::Schema.define(version: 20180227164123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20180227140322) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "driver_profiles", force: :cascade do |t|
