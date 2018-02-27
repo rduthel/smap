aa56e1ac8211166064db655a1a0acdb8748e6a81
Romain Duthel <romain.duthel@hotmail.fr>
2018-02-27 10:54:15 +0100

    isolated the two parts of result

--- db/scrap_avis.rb
+++ db/scrap_avis.rb
@@ -7,10 +7,16 @@ page_avis_prestige = open('https://www.avis.fr/services-avis/v%C3%A9hicules-de-l
 html_prestige = Nokogiri::HTML(page_avis_prestige)
 # html_tourism  = Nokogiri::HTML(page_avis_tourism)
 
+places    = []
+mecanique = []
+
 html_prestige.search('.content-51b').each do |element|
   # element.search('h2').each { |e| p e.text.split(':').last.strip }                  # nom de la voiture
   # element.search('.changeFontSize').each { |e| p e.text.split("\s\s").first.strip } # description
-  element.search('tr').each do |tr|
-    p tr.text.gsub(/\A[[:space:]]+/, '').split(' ').reject { |f| f.gsub(/\A[[:space:]]+/, '') == '' }
+  element.search('tr').each_with_index do |tr, index|
+    res = tr.text.gsub(/\A[[:space:]]+/, '').split(' ').reject { |f| f.gsub(/\A[[:space:]]+/, '') == '' }
+    index.even? ? places << res : mecanique << res
   end
 end
+
+p places
