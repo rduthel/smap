4b665fa2358b4981083936691c268454dcac4bc3
thomas jouvel <thomas.jouvel@gmail.com>
2018-03-02 17:27:10 +0100

    added a fonctionnal estimation page

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
-  skip_before_action :authenticate_user!
-  
+  skip_before_action :authenticate_user!, only: [:home, :index, :show, :estimation]
+
   def home
     @cars = Car.all
   end
@@ -13,7 +13,18 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
-  def estimation; end
+  def estimation
+    @car = Car.find(params[:id])
+  end
 
-  def subscription; end
+  def souscription
+    @car = Car.find(params[:id])
+    @driver_profile = DriverProfile.find_by user: current_user
+    @driver_profile.car = @car
+    if @driver_profile.save
+      redirect_to dashboard_path
+    else
+      render :estimation
+    end
+  end
 end
