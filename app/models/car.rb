5ca4e80b0d502ff2080dfec97be4132f2c893cf9
thomas jouvel <thomas.jouvel@gmail.com>
2018-03-05 11:14:56 +0100

    WIP

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,11 @@
 class Car < ApplicationRecord
   has_many :slots, dependent: :destroy
   has_many :ratings
+
+  include AlgoliaSearch
+
+  algoliasearch do
+    # all attributes will be sent
+  end
 end
 
---
+++ config/initializers/algoliasearch.rb
@@ -0,0 +1,1 @@
+AlgoliaSearch.configuration = { application_id: 'AVGP7PHOB1', api_key: 'dfa7a7611e062ed75c72d7cfe0572ea7' }
