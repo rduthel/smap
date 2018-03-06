bb34b67c81bae1b15e0d5a0f4c5e8dd79deb81c6
Romain Duthel <romain.duthel@hotmail.fr>
2018-03-06 17:00:02 +0100

    gets the start and end dates in a readable format

--- app/controllers/slots_json_controller.rb
+++ app/controllers/slots_json_controller.rb
@@ -1,6 +1,8 @@
 class SlotsJsonController < ApplicationController
   def create
-    p params
     p '--------------------- Coucou je suis dans slotsJSON ---------------------'
+    p Time.at(params['start'].to_i / 1000).to_datetime
+    p Time.at(params['end'].to_i / 1000).to_datetime
+
   end
 end
