fe43a5e00f1540c89d37d7265afd5c172b5cde1c
Romain Duthel <romain.duthel@hotmail.fr>
2018-03-06 17:28:36 +0100

    correctly adds a slot with the calendar, but FIXME: car and address are not getted by it

--- app/controllers/slots_json_controller.rb
+++ app/controllers/slots_json_controller.rb
@@ -1,8 +1,10 @@
 class SlotsJsonController < ApplicationController
   def create
-    p '--------------------- Coucou je suis dans slotsJSON ---------------------'
-    p Time.at(params['start'].to_i / 1000).to_datetime
-    p Time.at(params['end'].to_i / 1000).to_datetime
-
+    Slot.create!(
+      address_id: Address.first.id,
+      car_id: Car.first.id,
+      from: Time.at(params['start'].to_i / 1000).to_datetime,
+      to: Time.at(params['end'].to_i / 1000).to_datetime
+    )
   end
 end
