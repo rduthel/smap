ceb1591261dbe992d3b90b8fe0861b5db8346d3b
Romain Duthel <romain.duthel@hotmail.fr>
2018-03-07 14:44:19 +0100

    car is the same that the current_user's one

--- app/controllers/slots_json_controller.rb
+++ app/controllers/slots_json_controller.rb
@@ -2,7 +2,7 @@ class SlotsJsonController < ApplicationController
   def create
     Slot.create!(
       address_id: Address.where(name: params['title']).first.id,
-      car_id: Car.first.id,
+      car_id: set_driver_car.id,
       from: time_to_readable_date(params['start']),
       to: time_to_readable_date(params['end'])
     )
@@ -12,5 +12,10 @@ class SlotsJsonController < ApplicationController
 
   def time_to_readable_date(time)
     Time.at(time.to_i / 1000).to_datetime
+  end
+
+  def set_driver_car
+    @driver_profile = DriverProfile.find_by user: current_user
+    @car            = @driver_profile.car
   end
 end
