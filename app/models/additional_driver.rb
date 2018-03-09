4dc8c3eb00eca81824126f04fbcc0479c206dac6
thomas jouvel <thomas.jouvel@gmail.com>
2018-03-09 11:08:44 +0100

    fixed bug of additonnal driver

--- app/models/additional_driver.rb
+++ app/models/additional_driver.rb
@@ -5,10 +5,4 @@ class AdditionalDriver < ApplicationRecord
   mount_uploader :driving_license, PhotoUploader
   mount_uploader :identity_card, PhotoUploader
 
-  after_create :set_default_values
-
-  # def set_default_values
-  #   # Only set if photo IS NOT set
-  #   self.photo ||=   "profil.png"
-  # end
 end
